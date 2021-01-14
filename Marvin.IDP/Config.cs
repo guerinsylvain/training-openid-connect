ad054b53b8c19697f8a2b576170be2740646aa63
sguerin <sguerin@guess-engineering.com>
2021-01-14 17:35:03 +0100

    reference access token is now used

--- Marvin.IDP/Config.cs
+++ Marvin.IDP/Config.cs
@@ -46,49 +46,52 @@ namespace Marvin.IDP
                 new ApiResource("imagegalleryapi", "Image Gallery API")
                 {
                     Scopes = { "imagegalleryapi"},
-                    UserClaims = new List<string>() {"role"}
+                    UserClaims = new List<string>() {"role"},
+                    ApiSecrets = { new Secret("apisecret".Sha256())}
                 }
             };
 
         public static IEnumerable<Client> Clients =>
             new Client[]
             {  new Client
-            {
-                IdentityTokenLifetime = 300, // 300 seconds (this is the default value)
-                AuthorizationCodeLifetime =  300, // 300 seconds (this is the default value)
-                AccessTokenLifetime = 120,  // default is one hour
-                AllowOfflineAccess = true,
-                // AbsoluteRefreshTokenLifetime = ..., // default is 30 days
-                // RefreshTokenExpiration = TokenExpiration.Sliding,
-                // kSlidingRefreshTokenLifetime = .... // default is 15 days
-                UpdateAccessTokenClaimsOnRefresh = true,
-                ClientName = "Image Gallery",
-                ClientId = "imagegalleryclient",
-                AllowedGrantTypes = GrantTypes.Code,
-                RequireConsent = true,
-                RequirePkce = true,
-                RedirectUris = new List<string>()
                 {
-                    "https://localhost:44389/signin-oidc"
-                },
-                PostLogoutRedirectUris = new List<string>()
-                {
-                    "https://localhost:44389/signout-callback-oidc"
-                },
-                AllowedScopes =
-                {
-                    IdentityServerConstants.StandardScopes.OpenId,
-                    IdentityServerConstants.StandardScopes.Profile,
-                    IdentityServerConstants.StandardScopes.Address,
-                    "roles",
-                    "imagegalleryapi",
-                    "country",
-                    "subscriptionlevel"
-                },
-                ClientSecrets =
-                {
-                    new Secret("secret".Sha256())
+                    AccessTokenType = AccessTokenType.Reference,
+                    IdentityTokenLifetime = 300, // 300 seconds (this is the default value)
+                    AuthorizationCodeLifetime =  300, // 300 seconds (this is the default value)
+                    AccessTokenLifetime = 120,  // default is one hour
+                    AllowOfflineAccess = true,
+                    // AbsoluteRefreshTokenLifetime = ..., // default is 30 days
+                    // RefreshTokenExpiration = TokenExpiration.Sliding,
+                    // kSlidingRefreshTokenLifetime = .... // default is 15 days
+                    UpdateAccessTokenClaimsOnRefresh = true,
+                    ClientName = "Image Gallery",
+                    ClientId = "imagegalleryclient",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireConsent = true,
+                    RequirePkce = true,
+                    RedirectUris = new List<string>()
+                    {
+                        "https://localhost:44389/signin-oidc"
+                    },
+                    PostLogoutRedirectUris = new List<string>()
+                    {
+                        "https://localhost:44389/signout-callback-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Address,
+                        "roles",
+                        "imagegalleryapi",
+                        "country",
+                        "subscriptionlevel"
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    }
                 }
-            }};
+            };
     }
 }
